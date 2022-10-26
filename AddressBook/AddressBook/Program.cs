abf8d9e9b9fe91305b66c1b190b3e6b9dc5a6193
Mahesh7614 <waghmaremahesh012@gmail.com>
2022-10-26 15:14:01 +0530

    UC1 Address Book

--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace AddressBook
+﻿using AddressBook.Model;
+using AddressBook.Repository;
+using System;
+
+namespace AddressBook
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book");
+
+            ContactDetailsRepository contactDetailsRepository = new ContactDetailsRepository();
+            contactDetailsRepository.AddContactDetails("Mahesh", "Waghmare", 9028876143, "waghmaremahesh012@gmail.com", "Hari om Niwas, Nagoba Nagar", "Maharashtra", "Ahmedpur", 413515);
+            contactDetailsRepository.AddContactDetails("Yashwamt", "Kadam", 8856956513, "yashwant151197@gmail.com", "Holkar Nagar", "Maharashtra", "Parbhani", 431401);
         }
     }
 }
---
+++ AddressBook/AddressBook/Repository/ContactDetailsRepository.cs
@@ -0,0 +1,21 @@
+﻿using AddressBook.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook.Repository
+{
+    internal class ContactDetailsRepository
+    {
+        public Dictionary<long, ContactDetails> contactDetailDictionary = new Dictionary<long, ContactDetails>();
+
+        public void AddContactDetails(string firstName, string LastName, long mobileNumber, string email, string address, string state, string city, int zip)
+        {
+            ContactDetails contactDetails = new ContactDetails();
+            contactDetailDictionary.Add(mobileNumber, contactDetails);
+        }
+       
+    }
+}

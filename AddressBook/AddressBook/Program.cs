4a33224a6deb271f4c331587acd7ceb5311cb04c
Mahesh7614 <waghmaremahesh012@gmail.com>
2022-10-27 13:22:28 +0530

    UC2 Address Book

--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -1,18 +1,68 @@
 ﻿using AddressBook.Model;
 using AddressBook.Repository;
 using System;
+using System.Net;
 
 namespace AddressBook
 {
     internal class Program
-    {
+    {   
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book");
+            Console.WriteLine("Welcome to Address Book \n");
+            
+            ContactDetails contact = null;
+            
+            Console.WriteLine("Enter y to Enter Contact Details : ");
+            var input = Console.ReadLine();
+            
+            while (input == "y")
+            {
+                Console.WriteLine("Enter First Name : ");
+                string firstName = Console.ReadLine();
 
+                Console.WriteLine("Enter Last Name :");
+                string lastName = Console.ReadLine();
+
+                Console.WriteLine("Enter Mobile Number :");
+                long mobileNumber = Convert.ToInt64(Console.ReadLine());
+
+                Console.WriteLine("Enter Email ID : ");
+                string email = Console.ReadLine();
+
+                Console.WriteLine("Enter Address : ");
+                string address = Console.ReadLine();
+
+                Console.WriteLine("Enter City : ");
+                string city = Console.ReadLine();
+
+                Console.WriteLine("Enter State : ");
+                string state = Console.ReadLine();
+
+                Console.WriteLine("Enter Zip Code : ");
+                int zip = Convert.ToInt32(Console.ReadLine());
+                
+                Console.WriteLine("Enter y to Enter Contact Details or otherwise enter any key : ");
+                input = Console.ReadLine();
+
+                contact = new ContactDetails()
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    MobileNumber = mobileNumber,
+                    Email = email,
+                    Address = address,
+                    State = state,
+                    City = city,
+                    Zip = zip
+
+                };
+            }
+            
             ContactDetailsRepository contactDetailsRepository = new ContactDetailsRepository();
-            contactDetailsRepository.AddContactDetails("Mahesh", "Waghmare", 9028876143, "waghmaremahesh012@gmail.com", "Hari om Niwas, Nagoba Nagar", "Maharashtra", "Ahmedpur", 413515);
-            contactDetailsRepository.AddContactDetails("Yashwamt", "Kadam", 8856956513, "yashwant151197@gmail.com", "Holkar Nagar", "Maharashtra", "Parbhani", 431401);
+            contactDetailsRepository.AddContactDetails(contact);
+            contactDetailsRepository.DisplayContact();
+
         }
     }
 }
--- AddressBook/AddressBook/Repository/ContactDetailsRepository.cs
+++ AddressBook/AddressBook/Repository/ContactDetailsRepository.cs
@@ -11,11 +11,25 @@ namespace AddressBook.Repository
     {
         public Dictionary<long, ContactDetails> contactDetailDictionary = new Dictionary<long, ContactDetails>();
 
-        public void AddContactDetails(string firstName, string LastName, long mobileNumber, string email, string address, string state, string city, int zip)
+        public void AddContactDetails(ContactDetails contactDetails)
         {
-            ContactDetails contactDetails = new ContactDetails();
-            contactDetailDictionary.Add(mobileNumber, contactDetails);
+            contactDetailDictionary.Add(Convert.ToInt64(contactDetails.MobileNumber), contactDetails);
         }
-       
+        public void DisplayContact()
+        {
+            foreach (var item in contactDetailDictionary)
+            {
+                Console.WriteLine("First Name is : " + item.Value.FirstName);
+                Console.WriteLine("Last Name is : " + item.Value.LastName);
+                Console.WriteLine("Mobile Number is : " + item.Value.MobileNumber);
+                Console.WriteLine("Email ID is : " + item.Value.Email);
+                Console.WriteLine("Address is : " + item.Value.Address);
+                Console.WriteLine("State is : " + item.Value.State);
+                Console.WriteLine("City is : " + item.Value.City);
+                Console.WriteLine("Zip Code is : " + item.Value.Zip);
+                Console.WriteLine();
+            }
+        }
+
     }
 }
